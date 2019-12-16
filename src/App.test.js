370b1d3676ecff5d50676c925db84a1478d9d240
acehao94 <57552543+acehao94@users.noreply.github.com>
2019-12-16 21:04:24 +0800

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+newFunction();
+function newFunction() {
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+}
+
+newFunction();
+function newFunction() {
+  test('renders learn react link', () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+}
+
